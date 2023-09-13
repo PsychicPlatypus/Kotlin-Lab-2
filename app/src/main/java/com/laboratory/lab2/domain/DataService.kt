3c87c83b43f0abd529c522fab66ebbe12e9a7380
Dzenis Madzovic <dzenis.madzovic@pinmeto.com>
2023-09-13 16:47:06 +0200

    cleanup domain

--- app/src/main/java/com/laboratory/lab2/domain/DataService.kt
+++ app/src/main/java/com/laboratory/lab2/domain/DataService.kt
@@ -1,30 +1,36 @@
 package com.laboratory.lab2.domain
 
 import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.gson.Gson
 import com.laboratory.lab2.domain.models.Companies
 
 class DataService(private val documentId: String) {
     private val db = Firebase.firestore
 
-    fun getCompaniesData(): Task<Companies> {
+    fun getCompaniesData(): Task<Companies?> {
         return db.collection("companies")
             .document(documentId)
             .get()
             .continueWith { task ->
-                val result = task.result
-                if (result != null) {
-                    val companies = result.toObject(Companies::class.java)
-                    if (companies?.companies != null) {
-                        companies
-                    } else {
-                        Companies(emptyList())
-                    }
-                } else {
-                    Companies(emptyList())
-                }
+                handleCompaniesDataTask(task)
             }
     }
+
+    private fun handleCompaniesDataTask(task: Task<DocumentSnapshot>): Companies? {
+        if (!task.isSuccessful) {
+            return Companies(emptyList())
+        }
+        val result = task.result
+        if (result != null) {
+            val gson = Gson()
+            val companies = gson.toJson(result)
+            return gson.fromJson(companies, Companies::class.java)
+        }
+        return Companies(emptyList())
+    }
+
 
 }
